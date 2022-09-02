0b232cf203a19354846d376d1eb0a1c50edd8ae5
ashley <ashley.oketo@student.moringaschool.com>
2022-09-02 16:23:23 +0300

    Completed

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
 const apiHost = "https://localhost:5000"
 
-fetch(`${apiHost}`)
-.then(result =>result.json())
-.then(function onFormSubmit(){
-    //check validity
-    if (validate()){
-   //store user data
-   let formData = readFormData();
-   //check empty row
-   if (selectedRow === null)
-   {
-    // InsertNewRecord
-    insertNewRecord(formData);
-   }else {
-    updateNewRecord(formData)
-   }
-   resetForm();
+const form = document.getElementById("studentForm")
+console.log(form);
+
+form.addEventListener("submit",handleFormSubmit)
+
+function handleFormSubmit(event){
+    event.preventDefault()
+
+    const myInputs = {
+        username: event.target.username.value,
+        IDNumber:event.target.IDNumber.value,
+        Email:event.target.Email.value,
+        Class:event.target.Class.value,
+        RollNo:event.target.RollNo.value,
+        SponserName:event.target.SponserName.value,
+
     }
+    console.log(myInputs.username);
 }
-//Get values from form
-function readFormData(){
-    let formData = {};
-
-    //get the values from form
-    formData["Username"] = document.getElementById("Username").value;
-    formData["idNumber"] = document.getElementById("idNumber").value;
-    formData["Email"] = document.getElementById("Email").value;
-    formData["Class"] = document.getElementById("Class").value;
-    formData["rollNo"] = document.getElementById("rollNo").value;
-    formData["sponserName"] = document.getElementById("sponserName").value;
-
-    return formData;
-}
-)
+handleFormSubmit();
